c7e6b06d3d93037ee45ce1d6272dc371ec823f1d
Sergio Molinero <sergimol@ucm.es>
2022-04-05 00:10:40 +0200

    Escena ejecutable desde motor

--- DondeEstaLaPutaLeche/Src/StartScene.cpp
+++ DondeEstaLaPutaLeche/Src/StartScene.cpp
@@ -12,22 +12,24 @@
 #include <AnimatorController.h>
 #include <SinbadAnimTest.h>
 #include <iostream>
+#include <HornoVector3.h>
+#include <FactoryCreator.h>
 
 namespace El_Horno {
 	void StartScene::init()
 	{
 		Entity* light = addEntity("light", "prueba");
-		light->addComponent<Transform>("transform", Ogre::Vector3(0, 200, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0));
-		light->addComponent<LightComponent>("light", 0, Ogre::Vector3(0, 0, 0));
+		light->addComponent<Transform>("transform", HornoVector3(0, 200, 0), HornoVector3(0, 0, 0), HornoVector3(0, 0, 0));
+		light->addComponent<LightComponent>("light", 0, HornoVector3(0, 0, 0));
 
 		Entity* a = addEntity("camera", "prueba");
-		Ogre::Vector3 p = { 10,10,10 };
-		a->addComponent<Transform>("transform", Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(0, 0, 0));
-		a->addComponent<CameraComponent>("camera", Ogre::Vector3(0, 100, 450), Ogre::Vector3(0, 0, 0), Ogre::ColourValue(0, 0.3, 0.5), 5, 10000);
+		El_Horno::HornoVector3 p = { 10,10,10 };
+		a->addComponent<Transform>("transform", El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0, 0));
+		a->addComponent<CameraComponent>("camera", El_Horno::HornoVector3(0, 100, 450), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(0, 0.3, 0.5), 1, 5, 10000);
 		a->addComponent<AudioListenerComponent>("audioListener");
 
 		Entity* b = addEntity("object", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(0, 150, 0), Ogre::Vector3(0, 0, 0), p);
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(0, 150, 0), El_Horno::HornoVector3(0, 0, 0), p);
 		b->addComponent<Mesh>("mesh", "Sinbad");
 		b->addComponent<RigidBody>("rigidbody", 2.0f, false, false, 0);
 		b->addComponent<AnimatorController>("animatorController");
@@ -35,12 +37,12 @@ namespace El_Horno {
 		b->addComponent<AudioComponent>("audioComponent");
 
 		b = addEntity("object2", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(0, -15, 0), Ogre::Vector3(0, 0, 0), Ogre::Vector3(5, 0.1, 5));
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(0, -15, 0), El_Horno::HornoVector3(0, 0, 0), El_Horno::HornoVector3(5, 0.1, 5));
 		b->addComponent<Mesh>("mesh", "cube");
 		b->addComponent<RigidBody>("rigidbody", 0.0f, false, false, 0);
 
 		b = addEntity("esmoque", "prueba");
-		b->addComponent<Transform>("transform", Ogre::Vector3(250, 10, 0), Ogre::Vector3(0, 0, 0), p / 10);
+		b->addComponent<Transform>("transform", El_Horno::HornoVector3(250, 10, 0), El_Horno::HornoVector3(0, 0, 0), p);
 		b->addComponent<ParticleSystem>("particleSystem", "smoke", "Smoke", 10, true);
 		std::cout << "ESCENA CARGADA\n";
 	}
