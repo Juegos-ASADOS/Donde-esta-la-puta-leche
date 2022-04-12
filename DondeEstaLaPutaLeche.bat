32f8b8b49fdbe1ea769340042c64ecc4f3793184
Sergio Molinero <sergimol@ucm.es>
2022-04-12 11:35:55 +0200

    .bat para compilar

---
+++ DondeEstaLaPutaLeche.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+set EXES=.\Engine\ElHorno\Exes
+
+::cd ./DondeEstaLaPutaLeche/Engine
+::call ElHorno.bat
+::cd ../..
+
+cd ./DondeEstaLaPutaLeche
+msbuild DondeEstaLaPutaLeche.sln /p:configuration=Debug
+msbuild DondeEstaLaPutaLeche.sln /p:configuration=Release
+
+XCOPY /y /s .\Exes\ElHornoGame.dll %EXES%
+XCOPY /y /s .\Exes\ElHornoGame_d.dll %EXES%
+pause
