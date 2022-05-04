bec191c8dd2dcd706b93f7d53bafb0584028a588
diecasti <diecasti@ucm.es>
2022-05-04 16:57:20 +0200

    interfaz y menu
    de momento desde C, aprendimos a exponer funciones hasta cegui

--- DondeEstaLaPutaLeche/Src/UIMenus.cpp
+++ DondeEstaLaPutaLeche/Src/UIMenus.cpp
@@ -33,8 +33,68 @@ namespace El_Horno {
 		menu->getComponent<UILayout>("uilayout")->loadScheme("Generic");
 		//menu->getComponent<UILayout>("uilayout")->createButton("TaharezLook", "Button", "start");
 		menu->getComponent<UILayout>("uilayout")->addLayout("MenuPrincipal"); //Nombre del layout, y nombre interno cualquiera(que no se repita)
+		menu->getComponent<UILayout>("uilayout")->addLayout("MenuPrincipal_Opciones"); //Nombre del layout, y nombre interno cualquiera(que no se repita)
 		menu->getComponent<UILayout>("uilayout")->addLayout("Pausa"); //Nombre del layout, y nombre interno cualquiera(que no se repita)
-		menu->getComponent<UILayout>("uilayout")->addLayout("Carballo"); //Nombre del layout, y nombre interno cualquiera(que no se repita)
+		menu->getComponent<UILayout>("uilayout")->addLayout("Pausa_Opciones"); //Nombre del layout, y nombre interno cualquiera(que no se repita)
+
+
+		//añadir la funcionalidad a los botones
+
+		//menu principal
+		auto helperFunction = std::bind(&UIMenus::play_button, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal", "Menu/Boton", helperFunction);
+		helperFunction = std::bind(&UIMenus::opciones_button, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal", "Menu/Boton2", helperFunction);
+		helperFunction = std::bind(&UIMenus::creditos_button, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal", "Menu/Boton3", helperFunction);
+		helperFunction = std::bind(&UIMenus::salir_button, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal", "Menu/Boton4", helperFunction);
+
+		//menu opciones desde principal
+
+		helperFunction = std::bind(&UIMenus::restaMusica, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/RestaMusica", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaMusica, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/SumaMusica", helperFunction);
+		helperFunction = std::bind(&UIMenus::restaEfectos, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/RestaEfectos", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaEfectos, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/SumaEfectos", helperFunction);
+		helperFunction = std::bind(&UIMenus::restaResolucion, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/FlechaIz", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaResolucion, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/FlechaDer", helperFunction);
+		helperFunction = std::bind(&UIMenus::vuelveOpcionesMenu, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal_Opciones", "Menu/BotonVolver", helperFunction);
+
+		//menu opciones desde la pausa
+
+		helperFunction = std::bind(&UIMenus::restaMusica, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/RestaMusica", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaMusica, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/SumaMusica", helperFunction);
+		helperFunction = std::bind(&UIMenus::restaEfectos, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/RestaEfectos", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaEfectos, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/SumaEfectos", helperFunction);
+		helperFunction = std::bind(&UIMenus::restaResolucion, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/FlechaIz", helperFunction);
+		helperFunction = std::bind(&UIMenus::sumaResolucion, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/FlechaDer", helperFunction);
+		helperFunction = std::bind(&UIMenus::vuelveOpcionesPausa, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa_Opciones", "Menu/BotonVolver", helperFunction);
+
+		//Pausa
+
+		helperFunction = std::bind(&UIMenus::reanudarButton, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa", "Menu/BotonReanudar", helperFunction);
+		helperFunction = std::bind(&UIMenus::opcionesPausaButton, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa", "Menu/BotonOpciones", helperFunction);
+		helperFunction = std::bind(&UIMenus::salirPausaButton, this, std::placeholders::_1);
+		menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("Pausa", "Menu/BotonSalir", helperFunction);
+
+		// helperFunction = std::bind(&UIMenus::play_button, this, std::placeholders::_1);
+		//menu->getComponent<UILayout>("uilayout")->subscribeChildEvent("MenuPrincipal", "Menu/Boton3", helperFunction);
 	}
 
 	void UIMenus::update()
@@ -58,6 +118,109 @@ namespace El_Horno {
 			hide("Carballo");
 		}
 	}
+
+
+
+	//botones que deberian ir en LUA
+
+	//menu principal
+	bool UIMenus::play_button(const CEGUI::EventArgs& e) {
+		hide("MenuPrincipal");
+		return true;
+	}
+
+	bool UIMenus::opciones_button(const CEGUI::EventArgs& e) {
+
+		hide("MenuPrincipal");
+		show("MenuPrincipal_Opciones");
+		return true;
+	}
+
+	bool UIMenus::creditos_button(const CEGUI::EventArgs& e) {
+
+		hide("MenuPrincipal");
+		show("Creditos");
+		return true;
+	}
+
+
+	bool UIMenus::salir_button(const CEGUI::EventArgs& e) {
+
+		//pues decirle a todo el cacharro que se salga no tiene mas
+		hide("MenuPrincipal");
+		return true;
+
+	}
+
+	//menu opciones, para los dos salvo que sea el del opciones pausa
+	bool UIMenus::restaMusica(const CEGUI::EventArgs& e) {
+
+		std::cout << "resta musica\n";
+		return true;
+	}
+
+	bool UIMenus::sumaMusica(const CEGUI::EventArgs& e) {
+
+		std::cout << "suma musica\n";
+		return true;
+	}
+
+	bool UIMenus::restaEfectos(const CEGUI::EventArgs& e) {
+
+		std::cout << "resta sonido efectos\n";
+		return true;
+	}
+
+	bool UIMenus::sumaEfectos(const CEGUI::EventArgs& e) {
+
+		std::cout << "suma sonido efectos\n";
+		return true;
+	}
+
+	bool UIMenus::restaResolucion(const CEGUI::EventArgs& e) {
+		std::cout << "baja resolucion\n";
+		return true;
+	}
+
+	bool UIMenus::sumaResolucion(const CEGUI::EventArgs& e) {
+
+		std::cout << "sube resolucion\n";
+		return true;
+	}
+
+	bool UIMenus::vuelveOpcionesMenu(const CEGUI::EventArgs& e) {
+		hide("MenuPrincipal_Opciones");
+		show("MenuPrincipal");
+		return true;
+	}
+
+	bool UIMenus::vuelveOpcionesPausa(const CEGUI::EventArgs& e) {
+		hide("Pausa_Opciones");
+		show("Pausa");
+		return true;
+	}
+
+	//menu de pausa
+
+	bool UIMenus::reanudarButton(const CEGUI::EventArgs& e) {
+
+		hide("Pausa");
+		//devolver el flow del juego
+		return true;
+	}
+
+	bool UIMenus::opcionesPausaButton(const CEGUI::EventArgs& e) {
+		hide("Pausa");
+		show("Pausa_Opciones");
+		return true;
+	}
+
+	bool UIMenus::salirPausaButton(const CEGUI::EventArgs& e) {
+		hide("Pausa");
+		return true;
+	}
+
+
 	void UIMenus::show(std::string m)
 	{
 		menu->getComponent<UILayout>("uilayout")->setLayoutVisibility(m, true);
--- DondeEstaLaPutaLeche/Src/UIMenus.h
+++ DondeEstaLaPutaLeche/Src/UIMenus.h
@@ -4,6 +4,8 @@
 
 #include <Component.h>
 
+#include "UIManager.h"
+
 namespace El_Horno {
 	class UIMenus : public Component
 	{
@@ -13,7 +15,29 @@ namespace El_Horno {
 		void start() override;
 		void update() override;
 
+
+		//botones de los menus que deberian de ir por LUA
+		bool play_button(const CEGUI::EventArgs& e);
+		bool opciones_button(const CEGUI::EventArgs& e);
+		bool creditos_button(const CEGUI::EventArgs& e);
+		bool salir_button(const CEGUI::EventArgs& e);
+		bool restaMusica(const CEGUI::EventArgs& e);
+		bool sumaMusica(const CEGUI::EventArgs& e);
+		bool restaEfectos(const CEGUI::EventArgs& e);
+		bool sumaEfectos(const CEGUI::EventArgs& e);
+		bool restaResolucion(const CEGUI::EventArgs& e);
+		bool sumaResolucion(const CEGUI::EventArgs& e);
+		bool vuelveOpcionesMenu(const CEGUI::EventArgs& e);
+		bool vuelveOpcionesPausa(const CEGUI::EventArgs& e);
+		bool reanudarButton(const CEGUI::EventArgs& e);
+		bool opcionesPausaButton(const CEGUI::EventArgs& e);
+
+		bool salirPausaButton(const CEGUI::EventArgs& e);
+
+
 	private:
+
+		
 
 		void show(std::string menu);
 		void hide(std::string menu);
