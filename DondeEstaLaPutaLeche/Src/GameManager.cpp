d946067da90ed236c9e53fa64516d73d79fd63ef
albemu07 <albemu07@ucm.es>
2022-05-05 21:35:48 +0200

    Auto stash before merge of "main" and "origin/main"

--- DondeEstaLaPutaLeche/Src/GameManager.cpp
+++ DondeEstaLaPutaLeche/Src/GameManager.cpp
@@ -22,19 +22,19 @@ El_Horno::GameManager::GameManager()
 {
 	gameTimer_ = new Timer();
 	gameState_ = GameState::MAINMENU;
-}
-
-El_Horno::GameManager::~GameManager()
-{
-	if (this == instance_) {
-		instance_ = nullptr;
-	}
-	else {
-		delete instance_;
-	}
-	
-	if (gameTimer_ != nullptr)
-		delete gameTimer_; gameTimer_ = nullptr;
+}
+
+El_Horno::GameManager::~GameManager()
+{
+	if (this == instance_) {
+		instance_ = nullptr;
+	}
+	else {
+		delete instance_;
+	}
+	
+	if (gameTimer_ != nullptr)
+		delete gameTimer_; gameTimer_ = nullptr;
 }
 
 GameManager* GameManager::getInstance()
@@ -102,21 +102,21 @@ void El_Horno::GameManager::update()
 {
 	//actualizar la ui del reloj
 	if (gameState_ == GameState::RUNNING) {
-
-		string var = "Nivel_Ingame";
-		LuaManager::getInstance()->pushString(var, "layout");
-		var = "Reloj/Texto_Reloj";
-		LuaManager::getInstance()->pushString(var, "child");
-
-
-		int tiempo = maxTime_ - gameTimer_->getTime();
-
-		var = (((tiempo / 60 < 10) ? "0" : "") + to_string(tiempo / 60) + ":" + ((tiempo % 60 < 10) ? "0" : "") + to_string((tiempo % 60)));
-
-		//std::cout << var <<"\n";
-
-		LuaManager::getInstance()->pushString(var, "hora");
-
+
+		string var = "Nivel_Ingame";
+		LuaManager::getInstance()->pushString(var, "layout");
+		var = "Reloj/Texto_Reloj";
+		LuaManager::getInstance()->pushString(var, "child");
+
+
+		int tiempo = maxTime_ - gameTimer_->getTime();
+
+		var = (((tiempo / 60 < 10) ? "0" : "") + to_string(tiempo / 60) + ":" + ((tiempo % 60 < 10) ? "0" : "") + to_string((tiempo % 60)));
+
+		//std::cout << var <<"\n";
+
+		LuaManager::getInstance()->pushString(var, "hora");
+
 		LuaManager::getInstance()->callLuaFunction("setLayoutWidgetText");
 	}
 
@@ -125,7 +125,7 @@ void El_Horno::GameManager::update()
 		//Game over
 		endingEggs_ = 0;
 
-		// Escena final sin puntuación
+		// Escena final sin puntuaciï¿½n
 		UIManager::getInstance()->setLayoutVisibility("Derrota", true);
 		gameState_ == GameState::MAINMENU;
 		list_.clear();
@@ -139,7 +139,7 @@ void El_Horno::GameManager::update()
 		if (wrongProducts_ <= maxProducts_ / 4)
 			endingEggs_++;
 
-		//Pasar a la escena de score teniendo en cuenta la puntuación (huevos)
+		//Pasar a la escena de score teniendo en cuenta la puntuaciï¿½n (huevos)
 
 		UIManager::getInstance()->setLayoutVisibility("Victoria", true);
 
@@ -180,8 +180,8 @@ void El_Horno::GameManager::setLevel(float maxTime, std::map<std::string, int> l
 		gameState_ = GameState::MAINMENU;
 }
 
-// Busca la id en la lista, si está, resta uno a second. Si no está
-// o está a 0 devuelve false y añade un producto erroneo
+// Busca la id en la lista, si estï¿½, resta uno a second. Si no estï¿½
+// o estï¿½ a 0 devuelve false y aï¿½ade un producto erroneo
 bool El_Horno::GameManager::checkObject(std::string objectId)
 {
 	auto it = list_.find(objectId);
@@ -212,7 +212,7 @@ void El_Horno::GameManager::togglePaused()
 	}
 }
 
-// Comprueba si están todos los objetos en el carro
+// Comprueba si estï¿½n todos los objetos en el carro
 // para dar por valida la victoria
 void El_Horno::GameManager::checkEnd()
 {
